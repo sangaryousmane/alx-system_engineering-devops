40e0ab255ff73ee2c8fcd3de614a236bfc0404b2
sangaryousmane <sangaryousmane@gmail.com>
2023-10-03 09:37:54 -0700

    😎😎😎

---
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+# Using the provided cases
+# This is a ruby script that accepts one argument
+# and pass it to a regular expression matching method
+
+puts ARGV[0].scan(/hb?t?n/).join
