3a3d99deb44b943defb448dfe02a2533f895ed6a
sangaryousmane <sangaryousmane@gmail.com>
2023-11-24 06:09:23 -0800

    Creat files and install flask with puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+/* Create a file in /temp and set appropriate permissions */
+
+file { '/tmp/school':
+  ensure  => file,
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # alx-system_engineering-devops
 
 Please check the folder of your interest for the relevant documentation and usage of various files.
+0x0A-configuration_management
