7671e8d00f49f26d7b169eb4a4d1812e3aad77d9
sangaryousmane <sangaryousmane@gmail.com>
2023-11-30 19:29:14 -0800

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,4 @@
-# Using Puppet, install flask from pip.
-package { 'flask':
-  ensure => '2.1.0',
-  provider => 'pip3'
+# Using Puppet, install puppet lint.
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
 }
