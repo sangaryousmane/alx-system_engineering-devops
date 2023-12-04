5384271303ff1c04ab03506d5326f347e741fb0b
sangaryousmane <sangaryousmane@gmail.com>
2023-12-04 06:00:43 -0800

    Add a custom response header

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,29 @@
+# Add a custom HTTP header with Puppet
+
+exec { 'update the system: apt-get update':
+        command => '/usr/bin/apt-get update',
+}
+
+package { 'Install nginx':
+	ensure => 'installed',
+	require => Exec['update system']
+}
+
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
+}
+
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://th3-gr00t.tk/ permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+exec {'Set the HTTP header':
+	command => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+service {'run nginx':
+	ensure => running,
+	require => Package['nginx']
+}
