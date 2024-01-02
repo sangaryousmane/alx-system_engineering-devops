9aac6de98e00693878a1eb8a58f83c81233fc25a
sangaryousmane <sangaryousmane@gmail.com>
2024-01-02 01:25:00 -0800

    Export to csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-""" Export data to CSV
+""" Export data to csv
 """
-module = "0-gather_data_from_an_API"
-data = __import__(module)
-
 from requests import get
 import csv
 from sys import argv
 
 
 if __name__ == '__main__':
+    module = "0-gather_data_from_an_API"
+    data = __import__(module)
     employee_id = argv[1]
     employee_name, todos = data.get_data_from_api(employee_id)
 
@@ -18,7 +17,7 @@ if __name__ == '__main__':
 
         with open(file_name, "w", newline="") as to_file:
             csv_writer = csv.writer(to_file)
-            
+
             for task in todos:
                 csv_writer.writerow([
                     employee_id, employee_name,
