792d196529d2a9faff16869be0dad0dbdeeef115
sangaryousmane <sangaryousmane@gmail.com>
2024-01-02 18:46:08 -0800

    Formatting

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
 """ Export data to csv
 """
-from requests import get
 import csv
+from requests import get
 from sys import argv
 
 
 if __name__ == '__main__':
+    base_url = "https://jsonplaceholder.typicode.com/users"
     module = "0-gather_data_from_an_API"
     data = __import__(module)
     employee_id = argv[1]
-    employee_name, todos = data.get_data_from_api(employee_id)
+    _, todos = data.get_data_from_api(employee_id)
+    username = get(f'{base_url}/{employee_id}').json()
+    file_name = "{}.csv".format(employee_id)
 
-    if employee_name and todos:
-        file_name = "{}.csv".format(employee_id)
+    with open(file_name, "w", newline="") as to_file:
+        csv_writer = csv.writer(to_file)
 
-        with open(file_name, "w", newline="") as to_file:
-            csv_writer = csv.writer(to_file)
-
-            for task in todos:
-                csv_writer.writerow([
-                    employee_id, employee_name,
-                    "True" if task["completed"] else "False",
-                    task["title"]
-                    ])
+        for task in todos:
+            csv_writer.writerow([
+                employee_id, username,
+                "True" if task["completed"] else "False",
+                task["title"]
+                ])
