a158d5d0d8eed4b33898a9fa7d0517395d65ce6f
sangaryousmane <sangaryousmane@gmail.com>
2024-01-02 18:35:55 -0800

    Export to json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -12,17 +12,17 @@ if __name__ == '__main__':
     module = "0-gather_data_from_an_API"
     data = __import__(module)
     employee_id = argv[1]
-    employee_name, todos = data.get_data_from_api(employee_id)
+    _, todos = data.get_data_from_api(employee_id)
     emp_data = get(f"{users_url}/{employee_id}").json()
 
-    if employee_name and todos:
-        JSON_DATA = {
-                employee_id: [{
-                    "task": task["title"],
-                    "completed": task["completed"],
-                    "username": emp_data["username"]
-                    } for task in todos
-                    ]}
+    JSON_DATA = {
+            employee_id: [{
+                "task": task["title"],
+                "completed": task["completed"],
+                "username": emp_data["username"]
+                } for task in todos
+                ]}
     file_name = f"{employee_id}.json"
+
     with open(file_name, "w") as json_file:
         json.dump(JSON_DATA, json_file, indent=4)
