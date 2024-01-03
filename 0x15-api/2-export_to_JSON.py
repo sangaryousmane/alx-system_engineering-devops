c6bba532dee3b3d12ffd2e5bcd37ccdb2ef404a5
sangaryousmane <sangaryousmane@gmail.com>
2024-01-02 18:25:45 -0800

    Modify export to json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -8,19 +8,20 @@ from sys import argv
 
 
 if __name__ == '__main__':
+    users_url = "https://jsonplaceholder.typicode.com/users"
     module = "0-gather_data_from_an_API"
     data = __import__(module)
     employee_id = argv[1]
     employee_name, todos = data.get_data_from_api(employee_id)
+    emp_data = get(f"{users_url}/{employee_id}").json()
 
     if employee_name and todos:
         JSON_DATA = {
                 employee_id: [{
                     "task": task["title"],
                     "completed": task["completed"],
-                    "username": employee_name
+                    "username": emp_data["username"]
                     } for task in todos
-
                     ]}
     file_name = f"{employee_id}.json"
     with open(file_name, "w") as json_file:
